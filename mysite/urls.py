d74d2445045c4d1bf670489c112542e741de510c
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-24 22:36:50 +0430

    Edited

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Resume.urls'))
 ]
-re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
