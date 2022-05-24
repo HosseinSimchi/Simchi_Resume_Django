ccb365bfcb6a89b4968f74e1054baa8041608866
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-05-24 18:48:21 +0430

    done

--- Resume/urls.py
+++ Resume/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from Resume.views import index_view,about_view,contact_view
+from Resume.views import index_view,portfolio_view
 app_name = "Hossein"
 urlpatterns = [
     path('', index_view, name = "index"),
-    path('About', about_view, name = 'about'),
-    path('Contact', contact_view, name = 'contact')
+    path('About', portfolio_view, name = 'about')
 ]
 
